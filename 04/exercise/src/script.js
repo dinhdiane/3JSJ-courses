e43de3a89d1eeaa5b9ddbd68cd7547840b159702
Diane DINH <dianedinh@Dianes-MacBook-Pro.local>
2025-01-02 03:37:23 +0100

    transform objects

--- 04/exercise/src/script.js
+++ 04/exercise/src/script.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene()
 
 /**
  * Objects
- */
+ 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
@@ -26,6 +26,36 @@ mesh.rotation.reorder('YXZ')
 mesh.rotation.x = Math.PI * 0.25
 mesh.rotation.y = Math.PI * 0.25
 mesh.rotation.z = Math.PI * 0.25
+
+*/
+
+const group = new THREE.Group()
+group.position.set(1.5,0,-2)
+group.scale.set(1,1,2)
+group.rotation.reorder('YXZ')
+group.rotation.x = Math.PI * 0.25
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000})
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00})
+)
+cube2.position.set(-2, 0, 0)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff})
+)
+cube3.position.set(2, 0, 0)
+group.add(cube3)
+
 
 //Axes helper
 const axesHelper = new THREE.AxesHelper()
@@ -43,11 +73,11 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(0,1,3)
+camera.position.set(0,0,3)
 scene.add(camera)
 
 //Look at
-camera.lookAt(mesh.position)
+//camera.lookAt(mesh.position)
 
 /**
  * Renderer
